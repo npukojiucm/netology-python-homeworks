5cb887748f7d06381e17a4356dfe39190225f841
Aleksandr Platcev <a.platcev@ya.ru>
2025-03-23 13:46:50 +0300

    fix: revert to previous structure and fix README

--- Import. Module. Package/dirty_main.py
+++
@@ -1,8 +0,0 @@
-from application.salary import *
-from application.db.people import *
-from datetime import *
-
-if __name__ == '__main__':
-    print(datetime.now())
-    print(calculate_salary())
-    print(get_employees())
